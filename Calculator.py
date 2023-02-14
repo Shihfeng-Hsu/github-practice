92e1ac9f0d837335cefbb697aea8e5d800145069
Billy Hsu <ga017281@gmail.com>
2023-02-14 09:07:27 +0800

    modify the Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,6 @@
-def Calculator():
+def Calculator(a,b,method):
+    if method == 'add':
+        result = a + b
+        print(result)
+        return result
     print("This is a Calculator(Fake)")
